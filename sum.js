2429ea7c6cee0989613f8f07a28e267692b61099
RainInApril <90890779+RainInApril@users.noreply.github.com>
2024-08-25 22:31:25 +1000

    Create application to perform mathematical operation

---
+++ sum.js
@@ -0,0 +1,12 @@
+/* ‘Simple mathematics’ program in node.js */
+console.log("Assignment1: Sum of two numbers in Node.js")
+
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+
+if (isNaN(num1) || isNaN(num2)) {
+    console.error("Please enter 2 valid numers as arguments.")
+} else {
+    const sum = num1 + num2;
+    console.log(`The sum of ${num1} + ${num2} is: ${sum}`)
+}
